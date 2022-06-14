74b3b1d2c874f9af6895a32bc50defbdb8d87364
Fábio Amorim <batista.amorim@hotmail.com>
2022-06-14 19:43:46 -0300

    add pointer of pointer example

--- basic/8-pointer.c
+++ basic/8-pointer.c
@@ -12,12 +12,12 @@ int main()
 
     printf("Value of x variable: %i\n", x); // show us the value of x.
     printf("Value of x variable in pointer: %i\n", *px); // show us the value that is in memory address px.
-    printf("Value of memory of x variable: %x\n", &x); // shou us the memory address.
-    printf("Value of pointer: %x\n", px); // show us the memory address.
+    printf("Value of memory of x variable: %d\n", &x); // shou us the memory address.
+    printf("Value of pointer: %d\n", px); // show us the memory address.
 
     *px = 100; // save the new value in memory address.
 
-    printf("The new x value: %d", x);
+    printf("The new x value: %d\n", x);
 
     /* Result:
     Value of x variable: 20
@@ -27,8 +27,20 @@ int main()
     The new x value: 100
     */
 
+    //-------------------------------------------------
+    // pointer of pointer
+
+    int **q;
+
+    q = &px;
+
+    printf("px memory address: %d\n", &px);
+    printf("Value of q: %d\n", q); // the value in q is px memory address.
+    printf("Value in *q: %d\n", *q);
+    printf("Value in **q: %d\n", **q);
+
     printf("\n\n");
-    system("pause");
+    //system("pause");
     return 0;
 
 }
