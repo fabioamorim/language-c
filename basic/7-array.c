18588a906c5dd5d12789546ffc65dc48798915c8
Fábio Amorim <batista.amorim@hotmail.com>
2022-07-05 10:12:53 -0300

    Add new initialization and access the values

--- basic/7-array.c
+++ basic/7-array.c
@@ -16,11 +16,28 @@ int main()
                 need to shift the existing elements. This will create a position for us to insert
                 the new element at the deisred position. If the position at which we want to add
                 an element is at the beginning, then the shifting operation is more expensive.
+    
+    
+        Initialization:
+
+        int A[5]             -  [?,?,?,?,?] garbage in index;
+        int A[5]={2,3,4,5,6} -  [2,3,4,5,6]
+        int A[5]={2,4}       -  [2,4,0,0,0]
+        int A[5]={0}         -  [0,0,0,0,0] 
+        int A[]={2,3,4,5,6}  -  [2,3,4,5,6]  
     */
 
     int vetor[5] = {1,2,3,4,5};
     int matriz[3][3];
     int i, j;
+
+    //Print the array value:
+    // By index
+    printf("%d\n",vetor[2]);
+    printf("%d\n", 2[vetor]);
+
+    // By pointer
+    printf("%d\n", *(vetor+3));
 
     for (i=0;i<5;i++) {
         printf("Matriz: %i\n", vetor[i]);
